c25bb3a7f08f8d017f0f06f3deccc6080572cfec
brunomalheiros <brunopma10@gmail.com>
2021-02-24 17:02:30 -0300

    configurando o redirect no arquivo de views

--- achadoseperdidos/views.py
+++ achadoseperdidos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Objeto
 from .forms import ObjetoForm
 
@@ -13,7 +13,7 @@ def adicionaritem(request):
         if form.is_valid():
             objeto = form.save(commit=False)
             objeto.save()
-            return redirect('listadeintens')
+            return redirect('lista-de-itens')
     else:
         form = ObjetoForm()
     return render(request, 'adicionaritem.html', {'form': form})
