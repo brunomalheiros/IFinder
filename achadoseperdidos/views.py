63f1c915937394eb629b79334df71b8ddcd2c397
brunomalheiros <brunopma10@gmail.com>
2021-02-24 16:10:28 -0300

    atualizando as views

--- achadoseperdidos/views.py
+++ achadoseperdidos/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def home(request):
+    if request.method == 'POST':
+        form = ObjetoForm(request.POST, request.FILES)
+        if form.is_valid():
+            objeto = form.save(commit=False)
+            objeto.save()
+            return redirect('listadeintens')
+    else:
+        form = ObjetoForm()
     return render(request, 'index.html')
+
 
 def adicionaritem(request):
     return render(request, 'adicionaritem.html')
