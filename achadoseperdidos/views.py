bd727c4ad943530ac6b14b7d5f0e0c00887157c6
brunomalheiros <brunopma10@gmail.com>
2021-02-24 14:52:33 -0300

    atualizando o template index

--- achadoseperdidos/views.py
+++ achadoseperdidos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'templates/index.html')
+    return render(request, 'index.html')
 
 def adicionaritem(request):
     return render(request, 'adicionaritem.html')
