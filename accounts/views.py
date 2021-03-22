61919307233d8555a71d76bdb14464187d2bafd2
brunomalheiros <brunopma10@gmail.com>
2021-03-22 18:39:35 -0300

    adicionando as funções de login, cadastro e logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserForm, PerfilForm
 from django.contrib.auth import login as auth_login, authenticate, logout
+from django.contrib.auth.models import auth
+from django.contrib import messages
+from django.contrib.auth.models import User
+
 def cadastro(request):
-  
+    if request.user.is_authenticated:
+        return redirect('login')
+
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        form_perfil = PerfilForm(request.POST)
+
+        if form.is_valid() and form_perfil.is_valid():
+            user = form.save()
+            
+            perfil = form_perfil.save(commit=False)
+            perfil.usuario = user
+
+            perfil.save()
+
+            email = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=email, password=password)
+
+            auth_login(request, user)
+            return redirect('login')
+    else:
+        form = UserForm(request.POST)
+        form_perfil = PerfilForm(request.POST)
+
+    contexto = {
+        'form':form,
+        'form_perfil':form_perfil,
+    }
+
+    return render(request, 'cadastro.html', contexto)
+
 
 def login(request):
 
+    if request.user.is_authenticated:
+        return redirect('/listaitem')
 
-def logout_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/listaitem')
+        else:
+            messages.info(request, 'credenciais inválidas')
+            return redirect('login')
+    
+    else:
+        return render(request, 'login.html')
+    
+
+def logout(request):
     logout(request)
     return redirect('home')
-
 
