1d7360f412d54ea5207c2a26a2e7199c5cc4ac27
brunomalheiros <brunopma10@gmail.com>
2021-03-22 14:05:43 -0300

    configurando o arquivo de urls

--- achadoseperdidos/urls.py
+++ achadoseperdidos/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='index'),
     path('lista-de-itens', views.listadeitens, name='lista-de-itens'),
     path('adicionar-item', views.adicionaritem, name='adicionar-item'),
-    path('detalhes-do-item', views.item, name='detalhes-do-item'),
+    path('detalhes-do-item/<int:id>', views.item, name='detalhes-do-item'),
 ]
