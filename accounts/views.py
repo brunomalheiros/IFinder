f6849785202b1843b9b7aa262556fb5813bc4651
brunomalheiros <brunopma10@gmail.com>
2021-03-22 16:53:28 -0300

    configurando o arquivo de views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import login as auth_login, authenticate, logout
+def cadastro(request):
+  
 
-# Create your views here.
+def login(request):
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
