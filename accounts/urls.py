c86d4972d7039faf78c6ee19145bc52e14dba0e9
brunomalheiros <brunopma10@gmail.com>
2021-03-22 16:42:28 -0300

    atualizando o arquivo de urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,3 +4,4 @@ from django.urls import path
 urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout_view, name='logout'),
