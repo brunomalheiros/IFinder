25a478b79d327dda7d3e8cde9094e7fa110e1b92
brunomalheiros <brunopma10@gmail.com>
2021-03-22 18:30:36 -0300

    adicionando o arquivo models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Perfil(models.Model):
+    
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    turma = models.models.CharField('Turma', max_length=10)
+    whatsapp = models.CharField('Whatsapp', max_length=15)
+
+
+    def __str__(self):
+        return self.usuario.username
