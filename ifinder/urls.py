4b6e239ef8143db5a59e0959f201776d40fd7d30
brunomalheiros <brunopma10@gmail.com>
2021-03-24 16:05:08 -0300

    configurando a pasta de media

--- ifinder/urls.py
+++ ifinder/urls.py
@@ -20,4 +20,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('achadoseperdidos.urls')),
     path('accounts/', include('accounts.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
